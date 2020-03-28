bffc630983b44a91fae78d38e050f087c791d09d
Ioan <ioan.sava28@gmail.com>
2020-03-28 12:38:34 +0200

    Update

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -2,8 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.BookDto;
 import com.example.demo.service.BookService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +18,18 @@ public class BookController {
     @GetMapping("/book")
     public List<BookDto> getAllBooks() {
         return bookService.getAllBooks();
+    }
+
+    @PostMapping("/book")
+    public void addBook(@RequestBody BookDto bookDto) {
+        bookService.addBook(bookDto);
+    }
+
+    /*
+    localhost:8080/book/1?title=newTitle
+     */
+    @PutMapping("/book/{id}")
+    public void updateBookTitle(@PathVariable Long id, @RequestParam String title) {
+        bookService.updateBookTitle(id, title);
     }
 }
--- src/main/java/com/example/demo/service/BookService.java
+++ src/main/java/com/example/demo/service/BookService.java
@@ -4,7 +4,7 @@ import com.example.demo.Book;
 import com.example.demo.BookRepository;
 import com.example.demo.dto.BookDto;
 import org.modelmapper.ModelMapper;
-import org.springframework.stereotype.Service;;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,5 +23,22 @@ public class BookService {
         return ((List<Book>)  bookRepository.findAll()).stream()
                 .map(book -> modelMapper.map(book, BookDto.class))
                 .collect(Collectors.toList());
+    }
+
+    public void addBook(BookDto bookDto) {
+        bookRepository.save(modelMapper.map(bookDto, Book.class));
+    }
+
+    public void updateBookTitle(Long id, String title) {
+        bookRepository.updateBookTitle(id, title);
+
+        /*
+        //Alternativa
+        Optional<Book> book = bookRepository.findById(id);
+        if (book.isPresent()) {
+            book.get().setTitle(title);
+            bookRepository.save(book.get());
+        }
+         */
     }
 }
